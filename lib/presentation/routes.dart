04bed107f23a45e8a79837153c72ee4042d670b8
Haob2110011 <nhathaotad01@gmail.com>
2025-03-08 19:49:41 +0700

    Animation with PageRouteBuilder

--- lib/presentation/routes.dart
+++ lib/presentation/routes.dart
@@ -1,20 +1,41 @@
 import 'package:ct312hm01_temp/presentation/screens/history/history_screen.dart';
 import 'package:ct312hm01_temp/presentation/screens/guide/guide_screen.dart';
-import 'package:flutter/material.dart';
-
 import 'screens/auth/login_screen.dart';
 import 'screens/auth/register_screen.dart';
 import 'screens/chat/chat_screen.dart';
-
+import 'package:flutter/material.dart';
 
 Route<dynamic> mainRoute(RouteSettings settings) {
-  return switch (settings.name) {
-    ChatScreen.route => MaterialPageRoute(builder: (context) => ChatScreen()),
-    ChatHistoryScreen.route => MaterialPageRoute(builder: (context) => ChatHistoryScreen()),
-    GuideScreen.route => MaterialPageRoute(builder: (context) => GuideScreen()),
-    LoginScreen.route => MaterialPageRoute(builder: (context) => LoginScreen()),
-    RegisterScreen.route => MaterialPageRoute(builder: (context) => RegisterScreen()),
-    _ => MaterialPageRoute(builder: (context) => ChatScreen())
-  };
+  Widget page;
 
+  switch (settings.name) {
+    case ChatScreen.route:
+      page = ChatScreen();
+      break;
+    case ChatHistoryScreen.route:
+      page = ChatHistoryScreen();
+      break;
+    case GuideScreen.route:
+      page = GuideScreen();
+      break;
+    case LoginScreen.route:
+      page = LoginScreen();
+      break;
+    case RegisterScreen.route:
+      page = RegisterScreen();
+      break;
+    default:
+      page = ChatScreen();
+  }
+
+  return PageRouteBuilder(
+    settings: settings,
+    pageBuilder: (context, animation, secondaryAnimation) => page,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return FadeTransition(
+        opacity: animation,
+        child: child,
+      );
+    },
+  );
 }
